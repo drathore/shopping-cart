09be2c10d43400c260883fea031d53dbdfa7d5f2
Deepesh.Rathore <Deepesh.Rathore@mastek.com>
2016-11-23 23:27:03 +0000

    1st test passed. Added another failing test.

--- src/main/scala/ShoppingCartApp.scala
+++ src/main/scala/ShoppingCartApp.scala
@@ -4,7 +4,8 @@ object ShoppingCartApp extends App{
   print("Shopping cart...........")
   def checkout(shoppingCart: List[String]) : String = {
 
-    ""
+    val total = shoppingCart.size * 60
+    "£" + total/100.00
   }
 
 }
--- src/test/scala/ShoppingCartApp$Test.scala
+++ src/test/scala/ShoppingCartApp$Test.scala
@@ -5,8 +5,13 @@ import shoppingcart.ShoppingCartApp
 
 class ShoppingCartApp$Test extends FunSuite {
 
-  test("shopping cart with only apples calculated") {
+  test("shopping cart with only apples calculated correctly") {
     val shoppingCart = List[String]("Apple", "Apple")
     assert(ShoppingCartApp.checkout(shoppingCart) == "£1.2")
+  }
+
+  test("shopping cart with apples and oranges calculated correctly") {
+    val shoppingCart = List[String]("Apple", "Apple", "Orange", "Apple")
+    assert(ShoppingCartApp.checkout(shoppingCart) == "£2.05")
   }
 }
