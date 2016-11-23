d10703df64bd3bee6ad7bda56efbd6f4e3babc73
Deepesh.Rathore <Deepesh.Rathore@mastek.com>
2016-11-23 23:44:54 +0000

    Refactored code. Stage 1 complete.

--- src/main/scala/ShoppingCartApp.scala
+++ src/main/scala/ShoppingCartApp.scala
@@ -1,16 +1,44 @@
 package shoppingcart
 
 object ShoppingCartApp extends App{
+
   print("Shopping cart...........")
   def checkout(shoppingCart: List[String]) : String = {
+    val itemPrices: Map[String, Int] = Map( "Apple" -> 60, "Orange" -> 25)
+    val applesCount: Int = shoppingCart.count(i => i.equals("Apple"))
 
-    var sum = 0
-    for (item <- shoppingCart)  item match {
-      case "Apple" => sum = sum + 60
-      case "Orange" => sum = sum + 25
-    }
+    val orangesCount: Int = shoppingCart.count(i => i.equals("Orange"))
 
-    "£" + sum/100.00
+
+    val appleItem = new Item("Apple", itemPrices("Apple"), applesCount)
+
+    val orangeItem = new Item("Orange", itemPrices("Orange"), orangesCount)
+
+    val checkoutTotal = calculateTotalItemPrice(appleItem, new Calculator()) + calculateTotalItemPrice(orangeItem, new Calculator())
+
+    "£" + checkoutTotal/100.00
   }
 
+  def calculateTotalItemPrice(item: Item, calculator: ItemTotalCalculator): Int = {
+
+    val calculate: Int = calculator.calculate(item)
+    print(item.itemType + "===" + calculate)
+    return calculate
+  }
+
+}
+
+
+class Item(val itemType: String, val price: Int, val qty:Int){
+
+}
+
+trait ItemTotalCalculator {
+  def calculate(item: Item) : Int
+}
+
+class Calculator extends ItemTotalCalculator {
+  override def calculate(item: Item): Int = {
+    item.qty * item.price
+  }
 }
